29ee765a6f5ea2e8cbee6aebc1de0c093b8d811e
Tom Hamson <thomas.hamson@hamsoncreative.co.uk>
2020-05-23 15:55:05 +0100

    Initial commit.

---
+++ app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const startBtn = document.querySelector('.beer-button')
+  const randomBeer = document.querySelector('.random-beer')
+  const descriptionDisplay = document.querySelector('.description')
+
+  function getData() {
+    fetch('https://api.punkapi.com/v2/beers/random')
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      const name = data[0].name
+      const description = data[0].description
+      const {volume} = data[0]
+      const volumeValue = volume.value
+      const volumeUnit = volume.unit
+      
+      randomBeer.innerHTML = name + ' - ' + volumeValue + ' ' + volumeUnit
+      descriptionDisplay.innerHTML = description
+    })
+  }
+startBtn.addEventListener('click', getData)
+})
